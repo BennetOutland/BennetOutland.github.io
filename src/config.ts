4adc5fba944acc172e0ad049b29bede4f838ec8a
outlander1701 <bennet.outland@pm.me>
2025-07-28 19:30:25 -0600

    issue with config

--- src/config.ts
+++ src/config.ts
@@ -58,18 +58,6 @@ export const profileConfig: ProfileConfig = {
 	bio: "Doctoral canidate at CU Boulder. Interested in differential game theory, astrodynamics, and space robotics.",
 	links: [
 		{
-			// name: "Twitter",
-			// icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			// url: "https://twitter.com",
-		},
-		{
-			// name: "Steam",
-			// icon: "fa6-brands:steam",
-			// url: "https://store.steampowered.com",
-		},
-		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
 			url: "https://github.com/BennetOutland",
